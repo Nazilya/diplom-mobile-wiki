ba49ecd48ddf13a899f97a5cc44f5fcb32f8e01c
Nazilya <wkwad@mail.ru>
2023-02-19 15:43:54 +0300

    mobile-tests

--- src/test/java/org/wikipedia/ru/tests/NavBarMenuTests.java
+++ src/test/java/org/wikipedia/ru/tests/NavBarMenuTests.java
@@ -35,8 +35,9 @@ public class NavBarMenuTests extends TestBase {
         step("Tap NavBar menu button: Share", () ->
                 $(id("org.wikipedia.alpha:id/page_share")).click());
 
-        $$(id("android:id/text1")).shouldHave(texts("Bluetooth", "Drive", "Drive", "Messages",
-                "Bluetooth", "Drive", "Drive", "Gmail", "Google", "Messages"));
+        $$(id("android:id/text1")).shouldHave(texts("Nearby Share", "Android Beam",
+                "Bluetooth", "Copy to clipboard", "Gmail", "Messages",
+                "News Feed", "Save to Drive", "Your groups", "Your groups"));
 
 //        step("Check Share menu elements", () ->
 //                $$(id("android:id/text1")).shouldHave(texts("Bluetooth", "Gmail", "Messages", "Drive")));
@@ -69,7 +70,8 @@ public class NavBarMenuTests extends TestBase {
 
         step("Check list Item elements of submenu", () ->
                 $$(id("org.wikipedia.alpha:id/listItem")).shouldHave(texts("Save", "Language", "Find in article", "Theme", "Contents",
-                        "Share", "Watch", "Talk page", "Edit history", "New tab", "Explore")));
+                        "Share", "Watch", "Talk page", "Edit history", "New tab", "Explore", "Categories", "Edit article")));
+        //Categories, Edit article
     }
     @Owner("Nazilya")
     @Tag("mobile")
